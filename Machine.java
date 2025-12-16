5a6d4945ed5786218d519705d9be02241a512167
Sazk47 <jonathan212006hasard@gmail.com>
2025-12-16 08:47:10 +0100

    [ADD] adding Machine.java and Main.java for testing the coffee machine

---
+++ Machine.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Machine {
+
+    private int eau;
+    private int grainsCafe;
+    private int gobelets;
+    private int bacMarc;
+
+    private double monnayeur;
+    private double caisse;
+
+}
+
+   public Machine() {
+   }
