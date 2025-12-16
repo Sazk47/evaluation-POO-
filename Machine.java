4a44ca6e66df78fdae86579e7d3b68f1c3d338e1
Sazk47 <jonathan212006hasard@gmail.com>
2025-12-16 09:17:55 +0100

    [ADD] adding the products

--- Machine.java
+++ Machine.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 
 public class Machine {
 
+    private static final double PRIX_COURT = 1.50;
+    private static final double PRIX_LONG = 2.00;
+    
+    private static final int EAU_COURT = 10;
+    private static final int EAU_LONG = 20;
+    
+    private static final int GRAINS_CAFE = 10;
+    private static final int GOBELET = 1;
+    
+    private static final int DUREE_VIE = 5;
+
     private int eau;
     private int grainsCafe;
     private int gobelets;
@@ -23,23 +34,101 @@ public class Machine {
     return eau;
 }
 
-public int getGrainsCafe() {
+    public int getGrainsCafe() {
     return grainsCafe;
 }
 
-public int getGobelets() {
+    public int getGobelets() {
     return gobelets;
 }
 
-public int getBacMarc() {
+    public int getBacMarc() {
     return bacMarc;
 }
 
-public double getMonnayeur() {
+    public double getMonnayeur() {
     return monnayeur;
 }
 
-public double getCaisse() {
+    public double getCaisse() {
     return caisse;
+    }
+    
+    public void insererArgent(double montant) {
+        monnayeur = monnayeur + montant;
+    }
+    
+    private boolean estEntartree() {
+        return bacMarc >= DUREE_VIE;
+    }
+    
+    private String verifierDisponibilite(int eauNecessaire, int grainsNecessaires, double prixCafe) {
+        if (estEntartree()) {
+            return "ERREUR : Machine entartree - Appelez le technicien";
+        }
+        
+        if (monnayeur < prixCafe) {
+            return "Credit insuffisant, ajoutez de la monnaie";
+        }
+        
+        if (eau < eauNecessaire) {
+            return "Plus d'eau !";
+        }
+        if (grainsCafe < grainsNecessaires) {
+            return "Plus de grains de cafe !";
+        }
+        if (gobelets < 1) {
+            return "Plus de gobelets !";
+        }
+        
+        return "OK";
+    }
+    
+    public boolean servirCafeCourt() {
+        String verif = verifierDisponibilite(EAU_COURT, GRAINS_CAFE, PRIX_COURT);
+        
+        if (!verif.equals("OK")) {
+            System.out.println(verif);
+            return false;
+        }
+        
+        eau = eau - EAU_COURT;
+        grainsCafe = grainsCafe - GRAINS_CAFE;
+        gobelets = gobelets - GOBELET;
+        
+        caisse = caisse + PRIX_COURT;
+        monnayeur = monnayeur - PRIX_COURT;
+        
+        bacMarc = bacMarc + 1;
+        
+        System.out.println("Votre cafe est pret !");
+        return true;
+    }
+    
+    public boolean servirCafeLong() {
+        String verif = verifierDisponibilite(EAU_LONG, GRAINS_CAFE, PRIX_LONG);
+        
+        if (!verif.equals("OK")) {
+            System.out.println(verif);
+            return false;
+        }
+        
+        eau = eau - EAU_LONG;
+        grainsCafe = grainsCafe - GRAINS_CAFE;
+        gobelets = gobelets - GOBELET;
+        
+        caisse = caisse + PRIX_LONG;
+        monnayeur = monnayeur - PRIX_LONG;
+        
+        bacMarc = bacMarc + 1;
+        
+        System.out.println("Votre cafe est pret !");
+        return true;
+    }
+    
+    public double rendreMonnaie() {
+        double aRendre = monnayeur;
+        monnayeur = 0.0;
+        return aRendre;
     }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,34 @@
 public class Main {
 public static void main(String[] args) {
     Machine machine = new Machine();
-    System.out.println("Eau: " + machine.getEau());
-    System.out.println("Grains: " + machine.getGrainsCafe());
-    System.out.println("Gobelets: " + machine.getGobelets());
+    
+    System.out.println("=== TEST 1 : Cafe normal ===");
+    machine.insererArgent(2.0);
+    machine.servirCafeCourt();
+    System.out.println("Cafes servis: " + machine.getBacMarc());
+    
+    System.out.println("\n=== TEST 2 : Credit insuffisant ===");
+    machine.servirCafeLong();
+    
+    System.out.println("\n=== TEST 3 : Servir jusqu'a entartrage ===");
+    for (int i = 0; i < 4; i++) {
+        machine.insererArgent(2.0);
+        machine.servirCafeCourt();
+        System.out.println("Cafes servis: " + machine.getBacMarc());
+    }
+    
+    System.out.println("\n=== TEST 4 : Machine entartree ===");
+    machine.insererArgent(5.0);
+    machine.servirCafeCourt();
+    
+    System.out.println("\n=== TEST 5 : Verification des stocks ===");
+    Machine machine2 = new Machine();
+    machine2.insererArgent(20.0);
+    for (int i = 0; i < 6; i++) {
+        boolean ok = machine2.servirCafeLong();
+        if (!ok) {
+            break;
+        }
+    }
     }
 }
